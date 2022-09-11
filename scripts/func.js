829700e09ce7c2ab8a58dda3e3c665a9c8be9730
yu_十三 <13wl@sohu.com>
2022-09-11 21:46:46 +0800

    修复文章缩略图

--- scripts/func.js
+++ scripts/func.js
@@ -1,29 +1,30 @@
-'use strict'
+'use strict';
 // 随机数
-function randomNum (minNum, maxNum) {
+function randomNum(minNum, maxNum) {
   switch (arguments.length) {
     case 1:
-      return parseInt(Math.random() * minNum + 1)
-      break
+      return parseInt(Math.random() * minNum + 1);
+      break;
     case 2:
-      return parseInt(Math.random() * (maxNum - minNum + 1) + minNum)
-      break
+      return parseInt(Math.random() * (maxNum - minNum + 1) + minNum);
+      break;
     default:
-      return 1
-      break
+      return 1;
+      break;
   }
 }
 // 随机图片
-function random_bg () {
-  return '/assets/images/random/' + randomNum(1, 35) + '.jpg'
+function random_bg() {
+  return '/assets/images/random/' + randomNum(1, 35) + '.jpg';
 }
 // 获取文章图
-function content_img (content) {
-  if (!content) return random_bg()
+function content_img(content) {
+  const contentImg = this.url_for(random_bg());
+  if (!content) return contentImg;
 
-  const img = content.match(/<img.*src=[\"'](.*)[\"']/i)
-  if (!img) return random_bg()
-  return img[1]
+  const img = content.match(/<img.*src=[\"'](.*)[\"']/i);
+  if (!img) return contentImg;
+  return img[1];
 }
 
-hexo.extend.helper.register('content_img', content_img)
+hexo.extend.helper.register('content_img', content_img);
